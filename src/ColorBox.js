9cfd3b4cf632898373a43a42e3ec6aa2793c60ca
Dhanush MC <128697908+MCDhanush@users.noreply.github.com>
2023-10-12 17:44:03 +0530

    Update ColorBox.js

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,10 +1,10 @@
-// export function ColorBox({ color }) {
-//   const stylesB = {
-//     width: "20%",
-//     height: "25px",
-//     background: color,
-//     marginTop: '10px',
-//     key:"colors"
-//   };
-//   return <div style={stylesB}></div>;
-// }
+export function ColorBox({ color }) {
+  const stylesB = {
+    width: "20%",
+    height: "25px",
+    background: color,
+    marginTop: '10px',
+    key:"colors"
+  };
+  return <div style={stylesB}></div>;
+}
